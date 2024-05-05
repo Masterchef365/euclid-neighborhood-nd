c4c070efe45705641e596bec6a8c1fd613318bfc
Masterchef365 <duncan.freeman1@gmail.com>
2024-05-05 00:55:19 -0700

    Wow very short vecn

--- src/vecn.rs
+++ src/vecn.rs
@@ -58,22 +58,55 @@ impl<const DIMS: usize, T: Number> IndexMut<usize> for VecN<DIMS, T> {
     }
 }
 
-impl<const DIMS: usize, T: Number> Add for VecN<DIMS, T> {
-    type Output = Self;
-    fn add(self, rhs: Self) -> Self::Output {
-        self.merge(rhs, |a, b| a + b)
-    }
+macro_rules! math_op {
+    ($op_name:ident, $fname: ident, $code:expr) => {
+        // Implement operator for vectors
+        impl<const DIMS: usize, T: Number> $op_name for VecN<DIMS, T> {
+            type Output = Self;
+            fn $fname(self, rhs: Self) -> Self::Output {
+                self.merge(rhs, $code)
+            }
+        }
+
+        // Implement operator for scalars
+        impl<const DIMS: usize, T: Number> $op_name<T> for VecN<DIMS, T> {
+            type Output = Self;
+            fn $fname(self, rhs: T) -> Self::Output {
+                self.map(|v| (($code)(v, rhs)))
+            }
+        }
+    };
 }
+
+math_op!(Add, add, |a, b| a + b);
+math_op!(Sub, sub, |a, b| a - b);
+math_op!(Div, div, |a, b| a / b);
+math_op!(Mul, mul, |a, b| a * b);
+
 
 #[cfg(test)]
 mod tests {
     use super::VecN;
 
     #[test]
-    fn test_ivec3() {
+    fn test_ivec3_add() {
         let a: VecN<3, i32> = VecN([1, 2, 3]);
         let b: VecN<3, i32> = VecN([4, 5, 6]);
         let a_plus_b = a + b;
         assert_eq!(a_plus_b, VecN([5, 7, 9]));
+    }
+
+    #[test]
+    fn test_ivec3_add_scalar() {
+        let a: VecN<3, i32> = VecN([1, 2, 3]);
+        let a_plus_b = a + 8;
+        assert_eq!(a_plus_b, VecN([9, 10, 11]));
+    }
+
+    #[test]
+    fn test_vec3_add_scalar() {
+        let a: VecN<3, f32> = VecN([1., 2., 3.]);
+        let a_plus_b = a + 8.;
+        assert_eq!(a_plus_b, VecN([9., 10., 11.]));
     }
 }
